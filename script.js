279acdab400c5aa2ddaf584f50d0459121043b16
Linielt <115248908+Linielt@users.noreply.github.com>
2024-03-19 00:13:07 +0000

    Squares are clickable and text is added when clicked

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const Gameboard = (function () {
     const fillSquare = (num, naughtOrCross) => {
         if (grid[num] === "") {
             grid[num] = naughtOrCross;
+            // displayController.drawToSquare(num + 1, naughtOrCross);
         }
     }
 
@@ -30,21 +31,40 @@ const Gameboard = (function () {
 })();
 
 const GameManager = (function () {
+    let player1 = createPlayer("Player 1", "o"); // TODO - Allow user to add own names
+    let player2 = createPlayer("Player 2" , "x");
+    let currentTurn = player1;
+
     const start = () => {
-        let player1 = createPlayer("Player 1", "o"); // TODO - Allow user to add own names
-        let player2 = createPlayer("Player 2" , "x");
-        console.log(Gameboard.getGrid());
-        while (true) {
-            Gameboard.fillSquare(player1.chooseGridSpace(), player1.naughtOrCross);
-            console.log(Gameboard.getGrid());
-            if (checkForWin(player1)) break;
-            if (checkForDraw()) break;
-            Gameboard.fillSquare(player2.chooseGridSpace(), player2.naughtOrCross);
-            console.log(Gameboard.getGrid());
-            if (checkForWin(player2)) break;
-            if (checkForDraw()) break;
-        }
+        // console.log(Gameboard.getGrid());
+        // while (true) {
+        //     Gameboard.fillSquare(player1.chooseGridSpace(), player1.naughtOrCross);
+        //     console.log(Gameboard.getGrid());
+        //     if (checkForWin(player1)) break;
+        //     if (checkForDraw()) break;
+        //     Gameboard.fillSquare(player2.chooseGridSpace(), player2.naughtOrCross);
+        //     console.log(Gameboard.getGrid());
+        //     if (checkForWin(player2)) break;
+        //     if (checkForDraw()) break;
+        // }
+
+        // while (true) {
+        //     if (currentTurn == player1) { // Remove eventListener after clicking square
+                
+        //     }
+        // }
     };
+
+    const getCurrentTurn = () => currentTurn;
+
+    const switchTurns = () => {
+        if (currentTurn == player1) {
+            currentTurn = player2;
+        }
+        else if (currentTurn == player2) {
+            currentTurn = player1;
+        }
+    }
 
     const checkForWin = (player) => {
         let currentGrid = Gameboard.getGrid();
@@ -75,25 +95,31 @@ const GameManager = (function () {
         }
     }
 
-    return { start, checkForWin, checkForDraw };
+    return { start, getCurrentTurn, switchTurns, checkForWin, checkForDraw };
 })();
 
 function createPlayer (name, naughtOrCross) {
+
+    const gamesWon = 0;
+    const gamesLost = 0;
     
-    const chooseGridSpace = () => {
-        let chosenSquare;
-        while (true) {
-            chosenSquare = prompt(name + " Pick a square (1 - 9)");
-            if (chosenSquare < 1 || chosenSquare > 9 || Gameboard.isNSquareFilled(chosenSquare - 1, naughtOrCross)) {
-                alert("Invalid square");
-                continue;
-            }
+    // const chooseGridSpace = () => {
+    //     let chosenSquare;
+    //     while (true) {
+    //         chosenSquare = prompt(name + " Pick a square (1 - 9)");
+    //         if (chosenSquare < 1 || chosenSquare > 9 || Gameboard.isNSquareFilled(chosenSquare - 1, naughtOrCross)) {
+    //             alert("Invalid square");
+    //             continue;
+    //         }
 
-            return chosenSquare - 1;
-        }
-    }
+    //         return chosenSquare - 1;
+    //     }
+    // }
 
-    return { name, naughtOrCross, chooseGridSpace };
+    const incrementWins = () => gamesWon++;
+    const incrementLosses = () => gamesLost++;
+
+    return { name, naughtOrCross, incrementWins, incrementLosses };
 }
 
 const displayController = (function () {
@@ -110,13 +136,30 @@ const displayController = (function () {
 
         for (const square of gridContainer.children)
         {
-            square.addEventListener("click", () => {
-                square.innerHTML = "A";
-            });
+            square.addEventListener("click", fillSquare);
         }
     }
+
+    const fillSquare = (e) => {
+        if (GameManager.getCurrentTurn().naughtOrCross === "o") {
+            e.target.innerHTML = "o";
+        }
+        else if (GameManager.getCurrentTurn().naughtOrCross === "x") {
+            e.target.innerHTML = "x";
+        }
+
+        e.target.removeEventListener("click", fillSquare, false);
+    }
+
+    // const drawToSquare = (squareNum, naughtOrCross) => {
+    //     const square = document.getElementById("square_" + squareNum);
+    //     if (square.innerHTML == "") {
+    //         square.innerHTML = naughtOrCross;
+    //     }
+    // }
+
     return { instantiateGrid }
 })();
 
 displayController.instantiateGrid();
-// GameManager.start();
+GameManager.start();
